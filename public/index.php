969ecc64f180742a5338b016996538d0ccb661a4
hatone <b1006006@gmail.com>
2011-11-19 19:57:59 +0900

    fix

--- public/index.php
+++ public/index.php
@@ -23,8 +23,7 @@ define('PKGPATH', realpath(__DIR__.'/../fuel/packages/').DIRECTORY_SEPARATOR);
 /**
  * The path to the framework core.
  */
-define('COREPATH', realpath(__DIR__'../fuel/core/').DIRECTORY_SEPARATOR);
-
+define('COREPATH', realpath(__DIR__.'/../fuel/core/').DIRECTORY_SEPARATOR);
 
 // Get the start time and memory for use later
 defined('FUEL_START_TIME') or define('FUEL_START_TIME', microtime(true));
